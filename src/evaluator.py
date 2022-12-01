41b1ab865ac8c4a9451c43943b6c614294a3e64c
Keith Dao <keith.dao.248@gmail.com>
2022-12-01 21:26:04 +1000

    Update dataset transform to use predefined.

--- src/evaluator.py
+++ src/evaluator.py
@@ -9,28 +9,28 @@ class Evaluator:
     Evaluator for a given dataset and model.
     """
 
-    # Dataset
-    dataset_transform = torchvision.transforms.Compose(
-        [
-            torchvision.transforms.ToTensor(),
-        ]
-    )
-
-    # Model
     model_size_to_model = {
-        "tiny": torchvision.models.convnext_tiny,
-        "small": torchvision.models.convnext_small,
-        "base": torchvision.models.convnext_base,
-        "large": torchvision.models.convnext_large,
+        "tiny": (
+            torchvision.models.convnext_tiny,
+            torchvision.models.ConvNeXt_Tiny_Weights,
+        ),
+        "small": (
+            torchvision.models.convnext_small,
+            torchvision.models.ConvNeXt_Small_Weights,
+        ),
+        "base": (
+            torchvision.models.convnext_base,
+            torchvision.models.ConvNeXt_Base_Weights,
+        ),
+        "large": (
+            torchvision.models.convnext_large,
+            torchvision.models.ConvNeXt_Large_Weights,
+        ),
     }
 
     def __init__(self, dataset_path: str, model_size: str) -> None:
-        # Dataset
+        model, weights = Evaluator.model_size_to_model[model_size]
+        self.model = model(weights=weights.IMAGENET1K_V1)
         self.dataset = torchvision.datasets.ImageFolder(
-            dataset_path, transform=Evaluator.dataset_transform
-        )
-
-        # Model
-        self.model = Evaluator.model_size_to_model[model_size](
-            weights="DEFAULT"
+            dataset_path, transform=weights.IMAGENET1K_V1.transforms
         )
