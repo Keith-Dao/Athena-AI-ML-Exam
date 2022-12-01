c2b8fda0007d17adc2ebf8d67a35d21e41bcd8fb
Keith Dao <keith.dao.248@gmail.com>
2022-12-01 20:00:05 +1000

    Move argument parser creation to class.

--- test.py
+++ test.py
@@ -4,15 +4,22 @@
 import argparse
 
 
+class ArgumentParser(argparse.ArgumentParser):
+    """
+    Argument parser for this module.
+    """
+
+    def __init__(self, model_choices: list[str]) -> None:
+        super().__init__(
+            description="Evaluates a chosen model for the provided data.",
+            add_help=False,
+            usage=f"test.py path [--model {{ {', '.join(model_choices)} }}]",
+        )
+        self.add_argument("path")
+        self.add_argument("--model", choices=model_choices, default="tiny")
+
+
 if __name__ == "__main__":
     MODEL_SIZES = ["tiny", "small", "large", "base"]
-    arg_parser = argparse.ArgumentParser(
-        description="Evaluates a chosen model for the provided data.",
-        add_help=False,
-        usage=f"test.py path [--model {{ {', '.join(MODEL_SIZES)} }}]",
-    )
-    arg_parser.add_argument("path")
-    arg_parser.add_argument("--model", choices=MODEL_SIZES, default="tiny")
-
-    args = arg_parser.parse_args()
-    print(args)
+    parser = ArgumentParser(model_choices=MODEL_SIZES)
+    args = parser.parse_args()
