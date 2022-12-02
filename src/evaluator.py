7fcea69c2def27247d4e6c8041c5a818bb6b7727
Keith Dao <keith.dao.248@gmail.com>
2022-12-02 19:33:22 +1000

    Change to use model registration to get the model and weights.

--- src/evaluator.py
+++ src/evaluator.py
@@ -11,38 +11,24 @@ class Evaluator:
     Evaluator for a given dataset and model.
     """
 
-    model_size_to_model = {
-        "tiny": (
-            torchvision.models.convnext_tiny,
-            torchvision.models.ConvNeXt_Tiny_Weights,
-        ),
-        "small": (
-            torchvision.models.convnext_small,
-            torchvision.models.ConvNeXt_Small_Weights,
-        ),
-        "base": (
-            torchvision.models.convnext_base,
-            torchvision.models.ConvNeXt_Base_Weights,
-        ),
-        "large": (
-            torchvision.models.convnext_large,
-            torchvision.models.ConvNeXt_Large_Weights,
-        ),
-    }
     default_model_choice = "tiny"
-    model_choices = list(model_size_to_model.keys())
+    model_choices = ["tiny", "small", "base", "large"]
 
     def __init__(self, dataset_path: str, model_size: str) -> None:
-        model, weights = Evaluator.model_size_to_model[model_size]
         self.device = torch.device(
             "cuda:0" if torch.cuda.is_available() else "cpu"
         )
 
-        # Model
-        self.model = model(weights=weights.DEFAULT).to(self.device)
-
         # Data
+        data_transform = torchvision.models.get_weight(
+            f"ConvNeXt_{model_size.capitalize()}_Weights.DEFAULT"
+        ).transforms
         self.dataset = torchvision.datasets.ImageFolder(
-            dataset_path, transform=weights.DEFAULT.transforms()
+            dataset_path, transform=data_transform()
         )
-        self.data_iter = DataLoader(self.dataset, batch_size=32, shuffle=True)
+        self.loader = DataLoader(self.dataset, batch_size=32, shuffle=True)
+
+        # Model
+        self.model = torchvision.models.get_model(
+            f"convnext_{model_size}", weights="DEFAULT"
+        ).to(self.device)
