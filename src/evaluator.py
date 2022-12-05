eb416ff3e64dbd73f98c425dbaf32066ed172dd5
Keith Dao <keith.dao.248@gmail.com>
2022-12-05 18:47:00 +1000

    Add evaluator with confusion matrix plot.

---
+++ src/evaluator.py
@@ -0,0 +1,49 @@
+"""
+    Model and dataset evaluator module.
+"""
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from src.inferencer import Inferencer
+
+
+class Evaluator:
+    """
+    Evaluator for a given dataset and model.
+    """
+
+    def __init__(self, dataset_path: str, model_size: str) -> None:
+        self.inferencer = Inferencer(
+            dataset_path=dataset_path,
+            model_size=model_size,
+        )
+
+    def generate_confusion_matrix(self) -> None:
+        """
+        Generate the confusion matrix.
+        """
+        display_labels = self.inferencer.get_class_labels()
+        ConfusionMatrixDisplay.from_predictions(
+            *self.inferencer.get_confusion_matrix_data(),
+            labels=range(len(display_labels)),
+            display_labels=display_labels
+        )
+        plt.show(block=False)
+
+    def run(self) -> None:
+        """
+        Run the evaluation process.
+        """
+        print("Beginning evaluation.")
+
+        # Inference
+        self.inferencer.infer()
+
+        # Confusion matrix
+        self.generate_confusion_matrix()
+
+        # Block till all opened windows are closed.
+        print("Evaluation completed.")
+        print("Waiting for all windows to be closed.")
+        plt.show()
+        print("All windows closed. Exiting.")
--- src/inferencer.py
+++ src/inferencer.py
@@ -57,3 +57,23 @@ class Inferencer:
             self.confidences.append(prediction.values.item())
             self.predictions.append(prediction.indices.item())
         print("Inference completed.")
+
+    def get_confusion_matrix_data(self) -> tuple[list[int], list[int]]:
+        """
+        Gets the data needed to plot a confusion matrix.
+
+        Assumed that the inference step has been ran.
+
+        Returns:
+            A tuple of the actual labels and the predicted labels respectively
+        """
+        return self.dataset.targets, self.predictions
+
+    def get_class_labels(self) -> list[str]:
+        """
+        Gets the class labels.
+
+        Returns:
+            List of the class labels in the same order as the index
+        """
+        return self.dataset.classes
